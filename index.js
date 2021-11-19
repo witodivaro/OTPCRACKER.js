fc835a425b5c6347966dabe446c98967604da36f
witodivaro <witodivaro@gmail.com>
2021-11-19 09:53:48 +0300

    Decrypt all the messages

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function applyAsciiEncryptedHexesToString(string, ...asciiEncrypted) {
 }
 
 let key =
-  "66396e89c9dbd8cc9874352acd6395102eafce78aa7fed28a07f6bc98d29c50b69b0339a19f8aa401a9c6d708f80c066c763fef0123148cdd8e802d05ba98777335daefcecd59c433a6b268b60bf4ef03c9a61";
+  "66396e89c9dbd8cc9874352acd6395102eafce78aa7fed28a07f6bc98d29c50b69b0339a19f8aa401a9c6d708f80c066c763fef0123148cdd8e802d05ba98777335daefcecd59c433a6b268b60bf4ef03c9a611098bb3e9a3161edc7b804a33522cfd202d2c68c57376edba8c2ca50027c61246ce2a12b0c4502175010c0a1ba4625786d911100797d8a47e98b0204c4ef06c867a950f11ac989dea88fd1dbf16748749ed4c6f45b384c9d96c44d************************************************************************************************************";
 
 function showOne(key, index) {
   console.log();
